41ce8e060187b49a717fe13dc664cdfbacc05099
paesinh <paesinh_bai@163.com>
2019-05-24 10:36:00 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,28 @@
+// This is our App Service.
+// This is our data.
+var helloData = {
+  response: ''
+}
+// Register a Page.
+Page({
+  data: helloData,
+  httpRequest: function (e) { //对应：<button bindtap="httpRequest"> http test </button>的button点击
+    var that = this
+    wx.request({
+      url: 'https://www.paesinh.club/info.php', //仅为示例，要根据你实际的接口地址   
+      data: {
+        x: 'bai',
+        y: 'bai'
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data)
+        that.setData({
+          response: res.data //把接收到的服务器数据显示到{{response}}中
+        })
+      }
+    })
+  }
+})
